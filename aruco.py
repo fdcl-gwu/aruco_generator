1f6290d63fae419def85885d981b605ec117c820
Shankar Kulumani <shanks.k@gmail.com>
2017-05-11 17:29:06 -0400

    Add aruco generator

---
+++ aruco.py
@@ -0,0 +1,46 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+import cv2.aruco as aruco
+
+# Select type of aruco marker (size)
+aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_1000)
+print(aruco_dict)
+# second param is ID number
+# last param is total image size
+
+# Create an image from the marker
+img1 = aruco.drawMarker(aruco_dict, 1, 700)
+img2 = aruco.drawMarker(aruco_dict, 2, 700)
+# cv2.imwrite("test_marker.jpg", img)
+
+# create the image in matplotlib
+fig = plt.figure(frameon=False)
+fig.set_size_inches(6,6)
+
+# make content fill whole figure
+ax = plt.Axes(fig, [0, 0, 1, 1]) 
+ax.set_axis_off()
+fig.add_axes(ax)
+
+ax.imshow(img1, cmap='Greys_r')
+plt.savefig('aruco1.pdf')
+
+# another figure
+fig = plt.figure(frameon=False)
+fig.set_size_inches(6,6)
+
+# make content fill whole figure
+ax = plt.Axes(fig, [0, 0, 1, 1]) 
+ax.set_axis_off()
+fig.add_axes(ax)
+
+ax.imshow(img2, cmap='Greys_r')
+plt.savefig('aruco2.pdf')
+plt.show()
+# Display the image to us
+#cv2.imshow('frame', img)
+# Exit on any key
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
